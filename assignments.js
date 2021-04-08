1f6dc3c332ff708a3373a69c39d284f849955e93
Andrea <peric.andrea@nsoft.com>
2021-04-08 08:46:09 +0200

    first assignment

---
+++ assignments.js
@@ -0,0 +1,14 @@
+let randomNumber = Math.floor(Math.random() * 10 - 1) + 1
+
+if (randomNumber === 1) {
+    console.log(`And you have it, the number is ${randomNumber}`);
+} else {
+    const anotherNumber = randomNumber === 5;
+    console.log(`Now we have a number ${anotherNumber}`)
+} if (randomNumber > 8) {
+    console.log(`This number is somewhere in the middle, number ${randomNumber}`);
+} else {
+    console.log(`Or something else here.`);
+}
+
+
