c0b7ff680ef3a1861f321b34688ce2e926ecdea2
Andrea <peric.andrea@nsoft.com>
2021-04-29 14:16:19 +0200

    small const change

--- arrowFunction.js
+++ arrowFunction.js
@@ -2,7 +2,7 @@
 const yearsUntileRetirement = (birthYear, firstName) => {
 
     const age = 2047 - birthYear;
-    const retirement = 65;
+    const retirement = 65 - age;
     // return retirement;
     return `${firstName} retires in ${retirement} years.`;
 
