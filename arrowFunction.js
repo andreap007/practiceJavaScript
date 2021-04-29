e9a452cad6c1383f8d34bfe2709b3fff5554e48e
Andrea <peric.andrea@nsoft.com>
2021-04-29 13:37:06 +0200

    arrow function practice

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+
+const yearsUntileRetirement = (birthYear, firstName) => {
+
+    const age = 2047 - birthYear;
+    const retirement = 65;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`;
+
+}
+
+console.log(yearsUntileRetirement(1995, 'David'));
+console.log(yearsUntileRetirement(1989, 'Anna'));
