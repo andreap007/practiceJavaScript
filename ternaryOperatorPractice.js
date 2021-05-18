d7a24c37be85274be62582cbd80b54d4d8d133fe
Andrea <peric.andrea@nsoft.com>
2021-05-18 09:43:20 +0200

    minor changes array practice

--- ternaryOperatorPractice.js
+++ ternaryOperatorPractice.js
@@ -1,13 +1,13 @@
 
 // MARK: practice conditional ternary operator 
 
-/* const bill = 430;
+const bill = 430;
 const firstTip = bill * 15/100;
 const secondTip = bill * 20/100;
 const tip = bill >= 50 && bill <= 300 ? firstTip : secondTip; //get value with precentage 
 console.log(tip);
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`) */
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`)
 
 // Coding Challenge #2 functions and arrays
 
